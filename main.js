fa2950570e271a2b0baac4e6ee3562c798d6bc25
xSYDERx <ddpq.2804@gmail.com>
2024-02-29 20:16:51 +0100

    CSS terminado

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import './style.scss'
 
+const slider = document.querySelector('.slider');
+const sliderValue = document.querySelector('.slider-lenght');
+const uppercase = document.getElementById('uppercase');
+const lowercase = document.getElementById('lowercase');
+const numbers = document.getElementById('numbers');
+const symbols = document.getElementById('symbols');
+const strenght = document.querySelector('.strenght-status');
+const rect1 = document.getElementById('rectangle_one');
+const rect2 = document.getElementById('rectangle_two');
+const rect3 = document.getElementById('rectangle_three');
+const rect4 = document.getElementById('rectangle_four');
+const generate = document.getElementById('gen_button');
+
