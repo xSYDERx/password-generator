567e5e36d3bb5088a5aacedd016fb886e70cbe42
xSYDERx <ddpq.2804@gmail.com>
2024-02-29 22:00:01 +0100

    NPM RUN BUILD

--- main.js
+++ main.js
@@ -13,3 +13,29 @@ const rect3 = document.getElementById('rectangle_three');
 const rect4 = document.getElementById('rectangle_four');
 const generate = document.getElementById('gen_button');
 
+
+CHARACTER_SETS = {
+    uppercase: ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 26],
+    lowercase: ['abcdefghijklmnopqrstuvwxyz', 26],
+    numbers: ['1234567890', 10],
+    symbols: ['!@#$%^&*()', 10],
+  }
+
+getSliderVal = () => {
+    sliderValue.textContent = slider.value;
+  }
+
+
+styleRangeSlider = () => {
+    const min = slider.min;
+    const max = slider.max;
+    const val = slider.value;
+
+    slider.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+}
+
+slider.addEventListener('input', () => {
+  debugger
+    getSliderVal();
+    styleRangeSlider();
+});
